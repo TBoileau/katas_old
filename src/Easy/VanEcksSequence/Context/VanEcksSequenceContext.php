b681375a22ae5b32b0e2b52be81ff173c0675171
TBoileau <t-boileau@email.com>
2020-06-18 22:48:55 +0200

    test(easy): test Van Eck's Sequence

---
+++ src/Easy/VanEcksSequence/Context/VanEcksSequenceContext.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace TBoileau\CodinGame\Easy\VanEcksSequence;
+
+use Assert\Assertion;
+use Behat\Behat\Context\Context;
+use TBoileau\CodinGame\Easy\VanEcksSequence\UseCase\FindNumberInSequence;
+
+/**
+ * Class VanEcksSequenceContext
+ * @package TBoileau\CodinGame\Easy\VanEcksSequence
+ */
+class VanEcksSequenceContext implements Context
+{
+    /**
+     * @var FindNumberInSequence
+     */
+    private $findNumberInSequence;
+
+    /**
+     * @var int
+     */
+    private $start;
+
+    /**
+     * @var
+     */
+    private $position;
+
+    /**
+     * VanEcksSequenceContext constructor.
+     */
+    public function __construct()
+    {
+        $this->findNumberInSequence = new FindNumberInSequence();
+    }
+
+    /**
+     * @Given I start with :start
+     * @param int $start
+     */
+    public function iStartWith(int $start): void
+    {
+        $this->start = $start;
+    }
+
+    /**
+     * @When I've got the :position position of number that I need to find
+     * @param int $position
+     */
+    public function iHaveGotThePosition(int $position): void
+    {
+        $this->position = $position;
+    }
+
+    /**
+     * @Then I must find :number
+     * @param int $number
+     * @throws \Assert\AssertionFailedException
+     */
+    public function iMustFind(int $number): void
+    {
+        Assertion::eq($number, $this->findNumberInSequence->execute($this->start, $this->position));
+    }
+}
---
+++ src/Easy/VanEcksSequence/UseCase/FindNumberInSequence.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TBoileau\CodinGame\Easy\VanEcksSequence\UseCase;
+
+/**
+ * Class FindNumberInSequence
+ * @package TBoileau\CodinGame\Easy\VanEcksSequence\UseCase
+ */
+class FindNumberInSequence
+{
+    /**
+     * @param int $start
+     * @param $position
+     * @return int
+     */
+    public function execute(int $start, $position): int
+    {
+    }
+}
