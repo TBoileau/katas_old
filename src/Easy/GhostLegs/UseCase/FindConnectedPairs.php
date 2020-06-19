df30c59d38007c0dec1b2033adbfd3c3cbbf85a1
TBoileau <t-boileau@email.com>
2020-06-19 11:12:47 +0200

    feat: resolve Ghost Legs

--- src/Easy/GhostLegs/UseCase/FindConnectedPairs.php
+++ src/Easy/GhostLegs/UseCase/FindConnectedPairs.php
@@ -2,13 +2,29 @@
 
 namespace TBoileau\CodinGame\Easy\GhostLegs\UseCase;
 
+use TBoileau\CodinGame\Easy\GhostLegs\Entity\Diagram;
+
 /**
  * Class FindConnectedPairs
  * @package TBoileau\CodinGame\Easy\GhostLegs\UseCase
  */
 class FindConnectedPairs
 {
-    public function execute(): array
+    /**
+     * @param Diagram $diagram
+     * @return array
+     */
+    public function execute(Diagram $diagram): array
     {
+        $connectedPairs = [];
+        foreach ($diagram->getLines() as $line) {
+            $start = $line->getStart();
+            $path = $line;
+            for ($i = 0; $i < $diagram->getHeight(); $i++) {
+                $path = $path->to($i);
+            }
+            $connectedPairs[] = $start.$path->getEnd();
+        }
+        return $connectedPairs;
     }
 }
