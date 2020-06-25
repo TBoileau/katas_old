193834953cc4d6e0af298a7248d29e8f1e259fd1
TBoileau <t-boileau@email.com>
2020-06-25 14:58:16 +0200

    feat: resolve Tic Tac Toe

---
+++ src/Easy/TicTacToe/UseCase/FindWinningMove.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace TBoileau\CodinGame\Easy\TicTacToe\UseCase;
+
+use TBoileau\CodinGame\Easy\TicTacToe\Entity\Board;
+use TBoileau\CodinGame\Easy\TicTacToe\Entity\Cell;
+
+/**
+ * Class FindWinningMove
+ * @package TBoileau\CodinGame\Easy\TicTacToe\UseCase
+ */
+class FindWinningMove
+{
+    /**
+     * @param Board $board
+     * @return array
+     */
+    public function execute(Board $board): array
+    {
+        foreach ($board->getWinningCombinations() as $cells) {
+            $signs = array_map(function (Cell $cell) {
+                return $cell->getSign();
+            }, $cells);
+
+            $countSigns = array_count_values($signs);
+
+            if (($countSigns["."] ?? 0) === 1 && ($countSigns["O"] ?? 0) === 2) {
+                /** @var Cell $cell */
+                $cell = current(array_filter($cells, function (Cell $cell) {
+                    return $cell->getSign() === ".";
+                }));
+
+                $cell->setSign("O");
+
+                break;
+            }
+        }
+
+        return $board->toArray();
+    }
+}
